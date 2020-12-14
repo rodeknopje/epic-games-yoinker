636ae1e8cbd5e0fbeb38e53af2240a4768f37812
Rodeknopje <mn.stokhorst@gmail.com>
2020-12-14 18:10:34 +0100

    initial commit

---
+++ claimer/claimer/Program.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+
+namespace epic_claimer
+{
+    class Program
+    {
+        private static IWebDriver _driver;
+
+        private static WebDriverWait _wait;
+
+        private static void Main(string[] args)
+        {
+            if (args.Length < 3)
+            {
+                Console.WriteLine("not enough parameters");
+                
+                return;
+            }
+            
+            // create an instance of the webdriver
+            _driver = new ChromeDriver();
+            // create an instance of the webdriver waiter.
+            _wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(50));
+            // maximize the window.
+            _driver.Manage().Window.Maximize();
+            
+            // if the cookie was not retrieved successfully.
+            if (GetCookie(args[2]) == false)
+            {
+                Console.WriteLine("Failed to retrieve authentication cookie.");
+            
+                return;
+            }
+            
+            if (Login(args[0], args[1]) == false)
+            {
+                return;
+            }
+
+            Thread.Sleep(5000);
+            
+            foreach (var url in GetFreeGamesUrls())
+            {
+                ClaimGame(url);
+            }
+            
+            Console.WriteLine("process finished");
+        }
+
+        private static bool GetCookie(string url)
+        {
+            const int maxTries = 5;
+
+            _driver.Navigate().GoToUrl(url);
+
+            Thread.Sleep(5000);
+
+            for (var i = 0; i < maxTries; i++)
+            {
+                Console.Write($"{i + 1}/{maxTries} retrieving cookie : ");
+
+                GetElement("//button[@title=\"Click to set accessibility cookie\"]").Click();
+
+                Thread.Sleep(5000);
+
+                if (_driver.PageSource.Contains("Cookie set."))
+                {
+                    Console.WriteLine("success");
+
+                    return true;
+                }
+
+                Console.WriteLine("failed");
+            }
+
+            return false;
+        }
+
+        private static void AddEpicCookies()
+        {
+            _driver.Manage().Cookies.AddCookie(new Cookie(
+                name: "HAS_ACCEPTED_AGE_GATE_ONCE",
+                value: "true",
+                domain: "www.epicgames.com",
+                path: "/",
+                expiry: DateTime.Now.AddHours(1)
+            ));
+            _driver.Manage().Cookies.AddCookie(new Cookie(
+                name: "OptanonAlertBoxClosed",
+                value: "en-US",
+                domain: ".epicgames.com",
+                path: "/",
+                expiry: DateTime.Now.AddHours(1)
+            ));
+        }
+
+        private static bool Login(string user, string pass)
+        {
+            Console.Write("Logging in : ");
+
+
+            _driver.Navigate().GoToUrl("https://www.epicgames.com/id/login/");
+                
+            Thread.Sleep(5000);
+            
+            AddEpicCookies();
+            
+            GetElement("//div[@aria-label=\"Sign in with Epic Games\"]").Click();
+
+            GetElement("//input[@name=\"email\"]").SendKeys(user);
+            GetElement("//input[@name=\"password\"]").SendKeys(pass);
+
+            Thread.Sleep(5000);
+
+            GetElement("//span[text()=\"Log in now\"]").Click();
+
+            // wait for this element to be displayed
+            if (_wait.Until(x => x.FindElement(By.Id("egLogo")).Displayed))
+            {
+                Console.WriteLine("success");
+
+                return true;
+            }
+
+            Console.WriteLine("failed");
+
+            return false;
+        }
+
+        private static IEnumerable<string> GetFreeGamesUrls()
+        {
+            _driver.Navigate().GoToUrl("https://www.epicgames.com/store/en-US/free-games");
+
+            _wait.Until(x => x.FindElement(By.XPath("//div[@data-component=\"CardGridDesktopBase\"]")).Displayed);
+
+            Thread.Sleep(10000);
+
+            var urls = GetElements("//a[@aria-label[contains(., \"Free Games\")]]")
+                .Select(link => link.GetAttribute("href"))
+                .ToList();
+
+            return urls;
+        }
+
+        private static void ClaimGame(string url)
+        {
+            Console.Write($"claiming {url} : ");
+
+            _driver.Navigate().GoToUrl(url);
+
+            Thread.Sleep(5000);
+
+            if (_driver.PageSource.Contains("Owned</span>"))
+            {
+                Console.WriteLine("already owned");
+
+                return;
+            }
+            
+            // Click the get button.
+            GetElement("//button[@data-testid=\"purchase-cta-button\"]").Click();
+            Thread.Sleep(5000);
+            // Click place order button
+            GetElement("//button[@class=\"btn btn-primary\"]").Click();
+            Thread.Sleep(5000);
+            // click the agree button
+            GetElements("//button[@class=\"btn btn-primary\"]")[1].Click();
+
+            Thread.Sleep(5000);
+
+            Console.WriteLine("Claimed");
+        }
+
+        private static List<IWebElement> GetElements(string xPath)
+        {
+            _wait.Until(x => x.FindElements(By.XPath(xPath)));
+
+            return _driver.FindElements(By.XPath(xPath)).ToList();
+        }
+
+        private static IWebElement GetElement(string xPath)
+        {
+            _wait.Until(x => x.FindElement(By.XPath(xPath)));
+
+            return _driver.FindElement(By.XPath(xPath));
+        }
+    }
+}
